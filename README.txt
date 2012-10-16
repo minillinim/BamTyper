504c4337f1245cdb6e0d4436afc719f8e319d94f
Michael.Imelfort <mike@mikeimelfort.com>
2012-10-17 09:06:12 +1000

    oops

---
+++ README.txt
@@ -0,0 +1,16 @@
+===========
+BamTyper
+===========
+
+Provides functionality so that you can work out the orientation and insert
+size of a paired read data file. There's a few other bits and bobs which 
+could be used to produce rough scaffolds from mapped pairs
+
+Usage:
+
+    #!/usr/bin/env python
+    from bamtyper import properties
+
+    ### ADD MORE!
+
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='BamTyper',
+    version='0.0.1',
+    author='Michael Imelfort',
+    author_email='mike@mikeimelfort.com',
+    packages=['bamtyper'],
+    scripts=['bin/bamtyper.py'],
+    url='http://pypi.python.org/pypi/BamTyper/',
+    license='LICENSE.txt',
+    description='Working with paired reads in BAM format',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pysam >= 0.3",
+    ],
+)
