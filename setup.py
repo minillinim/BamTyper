7660a279d3d743eed982f2f066292a175f2ebe56
Michael.Imelfort <mike@mikeimelfort.com>
2013-07-11 23:20:33 +1000

    forgot to ship README.md to pip package
    versioning crud

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='BamTyper',
-    version='0.2.4',
+    version='0.2.5',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['bamtyper', 'bamtyper.test'],
     scripts=['bin/bamtyper'],
     url='http://pypi.python.org/pypi/BamTyper/',
-    license='LICENSE.txt',
+    license='GPLv3',
     description='Working with paired reads in BAM format',
     long_description=open('README.md').read(),
     install_requires=[
